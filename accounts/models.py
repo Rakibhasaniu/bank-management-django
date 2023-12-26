900033da986a4644931077e7f8ded0609edccf55
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-12-26 20:37:19 +0600

    user model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+ACCOUNT_TYPE = (
+    ('Savings','Savings'),
+    ('Current','Current'),
+)
 class UserBankAccount(models.Model):
-    
+    user = models.OneToOneField(User, related_name='account', on_delete = models.CASCADE)
+    account_type= models.CharField(max_length=50, choices=ACCOUNT_TYPE)
+    account_no= models.IntegerField(unique=True)
+    birth_date=models.DateField(null=True, blank=True)
