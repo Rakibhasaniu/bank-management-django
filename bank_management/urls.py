50e06616ed44729d28056d7ac7f637c473f8b7c4
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-12-28 01:15:36 +0600

    navbar created

--- bank_management/urls.py
+++ bank_management/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from core.views import HomeView
 urlpatterns = [
+    path('',HomeView.as_view()),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
