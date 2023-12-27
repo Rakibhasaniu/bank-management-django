f966c08c8b16eae0c655866b397e710919a7ae41
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-12-27 20:45:40 +0600

    FormView created

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .constants import ACCOUNT_TYPE,GENDER_TYPE
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserBankAccount,UserAddress
 # Create your
 
 class UserRegisterForm(UserCreationForm):
@@ -13,3 +14,31 @@ class UserRegisterForm(UserCreationForm):
     country=forms.CharField( max_length=50)
     class Meta:
         model= User
+        fields=['username','password','password2','first_name','last_name','email','account_type','birth_date','gender','postal_code','city','country','street_address']
+    def save(self,commit=True):
+        our_user=super().save(commit=False)
+        if commit == True:
+            our_user.save()
+            account_type=self.cleaned_data.get('account_type')
+            gender=self.cleaned_data.get('gender')
+            postal_code=self.cleaned_data.get('postal_code')
+            country=self.cleaned_data.get('country')
+            birth_date=self.cleaned_data.get('birth_date')
+            city=self.cleaned_data.get('city')
+            street_address=self.cleaned_data.get('street_address')
+            
+            UserAddress.objects.create(
+                user=our_user,
+                postal_code=postal_code,
+                country=country,
+                city=city,
+                street_address=street_address,
+            )
+            UserBankAccount.objects.create(
+                user=our_user,
+                account_type=account_type,
+                gender=gender,
+                birth_date=birth_date,
+                account_no=100000+our_user.id
+            )
+        return our_user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic import FormView
+from .forms import UserRegisterForm
+from django.contrib.auth import login,logout
 # Create your views here.
+
+
+class UserRegisterView(FormView):
+    template_name=''
+    form_class= UserRegisterForm
+    success_url=''
+    
+    
+    def form_valid(self, form):
+        user= form.save()
+        login(user)
+        return super().form_valid(form)
+    
+    
