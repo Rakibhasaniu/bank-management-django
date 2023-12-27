301e9e102a064fde8505ff4f018ef71b07e3d41e
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-12-27 23:08:54 +0600

    register page load successfully

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,40 +5,81 @@ from django.contrib.auth.models import User
 from .models import UserBankAccount,UserAddress
 # Create your
 
+# class UserRegisterForm(UserCreationForm):
+#     birth_date=forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+#     gender= forms.CharField(max_length=50, choices=GENDER_TYPE)
+#     street_address=forms.CharField( max_length=100)
+#     city=forms.CharField( max_length=50)
+#     postal_code=forms.IntegerField()
+#     country=forms.CharField( max_length=50)
+#     class Meta:
+#         model= User
+#         fields=['username','password','password2','first_name','last_name','email','account_type','birth_date','gender','postal_code','city','country','street_address']
+#     def save(self,commit=True):
+#         our_user=super().save(commit=False)
+#         if commit == True:
+#             our_user.save()
+#             account_type=self.cleaned_data.get('account_type')
+#             gender=self.cleaned_data.get('gender')
+#             postal_code=self.cleaned_data.get('postal_code')
+#             country=self.cleaned_data.get('country')
+#             birth_date=self.cleaned_data.get('birth_date')
+#             city=self.cleaned_data.get('city')
+#             street_address=self.cleaned_data.get('street_address')
+            
+#             UserAddress.objects.create(
+#                 user=our_user,
+#                 postal_code=postal_code,
+#                 country=country,
+#                 city=city,
+#                 street_address=street_address,
+#             )
+#             UserBankAccount.objects.create(
+#                 user=our_user,
+#                 account_type=account_type,
+#                 gender=gender,
+#                 birth_date=birth_date,
+#                 account_no=100000+our_user.id
+#             )
+#         return our_user
+
 class UserRegisterForm(UserCreationForm):
-    birth_date=forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    gender= forms.CharField(max_length=50, choices=GENDER_TYPE)
-    street_address=forms.CharField( max_length=100)
-    city=forms.CharField( max_length=50)
-    postal_code=forms.IntegerField()
-    country=forms.CharField( max_length=50)
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    gender = forms.ChoiceField(choices=GENDER_TYPE)
+    account_type = forms.ChoiceField(choices=ACCOUNT_TYPE)
+    street_address = forms.CharField(max_length=100)
+    city = forms.CharField(max_length= 100)
+    postal_code = forms.IntegerField()
+    country = forms.CharField(max_length=100)
     class Meta:
-        model= User
-        fields=['username','password','password2','first_name','last_name','email','account_type','birth_date','gender','postal_code','city','country','street_address']
-    def save(self,commit=True):
-        our_user=super().save(commit=False)
+        model = User
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'account_type', 'birth_date','gender', 'postal_code', 'city','country', 'street_address']
+        
+        # form.save()
+    def save(self, commit=True):
+        our_user = super().save(commit=False) # ami database e data save korbo na ekhn
         if commit == True:
-            our_user.save()
-            account_type=self.cleaned_data.get('account_type')
-            gender=self.cleaned_data.get('gender')
-            postal_code=self.cleaned_data.get('postal_code')
-            country=self.cleaned_data.get('country')
-            birth_date=self.cleaned_data.get('birth_date')
-            city=self.cleaned_data.get('city')
-            street_address=self.cleaned_data.get('street_address')
+            our_user.save() # user model e data save korlam
+            account_type = self.cleaned_data.get('account_type')
+            gender = self.cleaned_data.get('gender')
+            postal_code = self.cleaned_data.get('postal_code')
+            country = self.cleaned_data.get('country')
+            birth_date = self.cleaned_data.get('birth_date')
+            city = self.cleaned_data.get('city')
+            street_address = self.cleaned_data.get('street_address')
             
             UserAddress.objects.create(
-                user=our_user,
-                postal_code=postal_code,
-                country=country,
-                city=city,
-                street_address=street_address,
+                user = our_user,
+                postal_code = postal_code,
+                country = country,
+                city = city,
+                street_address = street_address
             )
             UserBankAccount.objects.create(
-                user=our_user,
-                account_type=account_type,
-                gender=gender,
-                birth_date=birth_date,
-                account_no=100000+our_user.id
+                user = our_user,
+                account_type  = account_type,
+                gender = gender,
+                birth_date =birth_date,
+                account_no = 100000+ our_user.id
             )
         return our_user
--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import UserRegisterForm
 from django.contrib.auth import login,logout
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 
 class UserRegisterView(FormView):
-    template_name=''
+    template_name='accounts/user_register.html'
     form_class= UserRegisterForm
-    success_url=''
+    success_url=reverse_lazy('register')
     
     
     def form_valid(self, form):
