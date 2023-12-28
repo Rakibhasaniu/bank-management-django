240b0733485be91aa2aedc2e0a07ded6520746c0
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-12-28 19:10:52 +0600

    logout and profile updated implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path,include
-from .views import UserRegisterView,UserLoginView,LogOutView
+from .views import UserRegisterView,UserLoginView,LogoutViewS,UserBankAccountUpdateView
 
 urlpatterns = [
     
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', LogOutView.as_view(), name='logout'),
+    path('logout/', LogoutViewS.as_view(), name='logout'),
+    path('profile/', UserBankAccountUpdateView.as_view(), name='profile'),
 ]
