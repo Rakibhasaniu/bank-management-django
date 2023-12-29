26c787c96b9a320115eef93c44ae6866ea6748d5
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-12-29 23:02:01 +0600

    deposite and withdraw view created

--- transactions/constants.py
+++ transactions/constants.py
@@ -1,8 +1,11 @@
 
-
+DEPOSITE=1
+WITHDRAW=2
+LOAN=3
+LOAN_PAID=4
 TRANSACTION_TYPE=(
-    (1,'Deposite'),
-    (2,'Withdraw'),
-    (3,'Loan'),
-    (4,'Loan Paid')
+    (DEPOSITE,'Deposite'),
+    (WITHDRAW,'Withdraw'),
+    (LOAN,'Loan'),
+    (LOAN_PAID,'Loan Paid')
 )
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,96 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
+from .forms import DepositeForm,WithdrawForm,LoanRequestForm
+from .constants import DEPOSITE,WITHDRAW,LOAN,LOAN_PAID
+from django.contrib import messages
 # Create your views here.
+
+
+class TransactionCreateMixin(LoginRequiredMixin,CreateView):
+    template_name=''
+    model = Transaction
+    title=''
+    success_url=''
+    
+    def get_form_kwargs(self):
+        kwargs=super().get_form_kwargs()
+        kwargs.update({
+            'account': self.request.user.account,
+        })
+        return kwargs
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': self.title
+        })
+
+
+class DepositeView(TransactionCreateMixin):
+    form_class=DepositeForm
+    title='Deposite'
+    
+    def get_initial(self):
+        initial={'transaction_type': DEPOSITE}
+        return initial
+    def form_valid(self, form):
+        amount=form.cleaned_data.get('amount')
+        account=self.request.user.account
+        account.balance += amount
+        account.save(
+            update_fields=['balance']
+        )
+        
+        messages.success(self.request),f"{amount}$ was deposited to your account successfully"
+        return super().form_valid(form)
+class WithdrawView(TransactionCreateMixin):
+    form_class=WithdrawForm
+    title='Withdraw Money'
+    
+    def get_initial(self):
+        initial={'transaction_type': WITHDRAW}
+        return initial
+    def form_valid(self, form):
+        amount=form.cleaned_data.get('amount')
+        account=self.request.user.account
+        account.balance -= amount
+        account.save(
+            update_fields=['balance']
+        )
+        
+        messages.success(self.request),f"successfully withdraw {amount}$ from your account"
+        return super().form_valid(form)
+class WithdrawView(TransactionCreateMixin):
+    form_class=LoanRequestForm
+    title='Request For Loan'
+    
+    def get_initial(self):
+        initial={'transaction_type': LOAN}
+        return initial
+    def form_valid(self, form):
+        amount=form.cleaned_data.get('amount')
+        current_loan_count=Transaction.objects.filter(account=self.request.user.account, transaction_type=3)
+        
+        
+        
+        messages.success(self.request),f"successfully withdraw {amount}$ from your account"
+        return super().form_valid(form)
+class WithdrawView(TransactionCreateMixin):
+    form_class=WithdrawForm
+    title='Withdraw Money'
+    
+    def get_initial(self):
+        initial={'transaction_type': WITHDRAW}
+        return initial
+    def form_valid(self, form):
+        amount=form.cleaned_data.get('amount')
+        account=self.request.user.account
+        account.balance -= amount
+        account.save(
+            update_fields=['balance']
+        )
+        
+        messages.success(self.request),f"successfully withdraw {amount}$ from your account"
+        return super().form_valid(form)
+
